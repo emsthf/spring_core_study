da3728c79a021ca93b5dc5fe18e589b7b9cecf7d
Deunsol Park <deunsol916@gmail.com>
2022-07-15 00:01:14 +0900

    FEAT: BeanDefinition으로 스프링 빈 메타정보 확인하기 1. 스프링 빈 메타정보 확인을 위한 테스트 코드 작성 2. 빈을 등록하는 2가지 방법(어노테이션, xml) 모두 테스트 3. xml로 빈을 등록하면 빈에 대한 클래스 정보가 밖에 드러난다 4. 어노테이션으로 등록하면 빈 클래스 정보가 null 4-1. 이는 factoryBean을 통해서 호출, 생성되는 방식

---
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    // 리턴 타입을 ApplicationContext로 하지 않는 이유 = getBeanDefinition을 하지 못하기 때문
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
